fe35a1883ca98ddc7bb74cd82740188ce4fbb5fd
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-28 09:46:33 +0100

    refactorisation + utilistion d'un do/while pour gérer la répétitivité du programme

--- src/Game.java
+++ src/Game.java
@@ -3,44 +3,52 @@ import java.util.Random;
 
 public class Game {
 
-	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Hello ! Do you want to play? (y or n)");
-		
-		Random random = new Random();
-		int target = random.nextInt(100)+1;
-		int counter = 0;
-		
-		while (scan.hasNext()) {
-			System.out.println("Please guess the target number, between 1 and 100: \n");
-			if (scan.hasNextInt()) {
-				int value = scan.nextInt();
-				if (value == target) {
-					counter++;
-					System.out.println("Congratulations! You find " + target + " in " + counter + " turns.");
-					System.out.println("Do you want to play again? (y or n)");
-					break;
-				}
-				if (value < target) {
-					System.out.println("The target number is higher! Please try again.");
-					counter++;
-				}
-				else {
-					System.out.println("The target number is lower! Please try again.");
-					counter++;
-				}
-			}
-			else {
-				String input = scan.next();
-				if (input.equalsIgnoreCase("n")) {
-					System.out.println("You are quitting the program! See you next time!");
-					break;
-				}
-			}
-		}
-		
-		scan.close();
-	}
+    public static void main(String[] args) {
+    	
+    	
+    	System.out.println("Welcome!");
+        Scanner scan = new Scanner(System.in);
+        String playAgain;
 
+        do {
+            Random random = new Random();
+            int target = random.nextInt(100) + 1;
+            int counter = 0;
+            boolean guessed = false;
+
+            System.out.println("New game started! Try to guess the number between 1 and 100: ");
+
+            while (!guessed) {
+                System.out.print("Your guess: ");
+                if (scan.hasNextInt()) {
+                    int value = scan.nextInt();
+                    counter++;
+                    if (value == target) {
+                        System.out.println("Congratulations! You found " + target + " in " + counter + " turns.");
+                        guessed = true;
+                    } else if (value < target) {
+                        System.out.println("The target number is higher! Try again.");
+                    } else {
+                        System.out.println("The target number is lower! Try again.");
+                    }
+                } else {
+                    String input = scan.next();
+                    if (input.equalsIgnoreCase("n")) {
+                        System.out.println("You are quitting the program! See you next time!");
+                        scan.close();
+                        return;
+                    } else {
+                        System.out.println("Please enter a valid number.");
+                    }
+                }
+            }
+
+            System.out.print("Do you want to play again? (y or n): ");
+            playAgain = scan.next();
+
+        } while (playAgain.equalsIgnoreCase("y"));
+
+        System.out.println("Thanks for playing! Goodbye.");
+        scan.close();
+    }
 }
